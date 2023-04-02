caa3a383d7dfe5de9d194782af5f835941155498
herdiardn <121550473+herdiardn@users.noreply.github.com>
2023-04-02 15:40:54 +0700

    Herdi (#1)
    * menambahkan class KuliahMahasiswa_1302210078
    
    ---------
    
    Co-authored-by: Kamal Maulaazka Sidhqi <kamalmlzka@student.telkomuniversity.ac.id>

--- tpmodul7_kelompok_5/Program.cs
+++ tpmodul7_kelompok_5/Program.cs
@@ -1,4 +1,22 @@
-﻿using System.Text.Json;
+using System.Text.Json;
+public class Program
+{
+    private static void Main(string[] args)
+    { 
+        KuliahMahasiswa_1302210078 KM = readJSON("tp7_2_1302210078.json");
+        for (int i = 0; i < KM.courses.Count; i++)
+        {
+            Console.WriteLine("MK " + (i+1) + " " + KM.courses[i].code + " - " + KM.courses[i].name);
+        }
+    }
+    
+    public static KuliahMahasiswa_1302210078 readJSON(string filename)
+    {
+        string posisifolder = "C:\\Users\\ASUS\\OneDrive - Telkom University\\Documents\\MAPEL RPL SEMESTER 4\\KPL\\tpmodul7_kelompok_5\\tpmodul7_kelompok_5";
+        string hasilbaca = File.ReadAllText(posisifolder + "\\" + filename);
+        return JsonSerializer.Deserialize<KuliahMahasiswa_1302210078>(hasilbaca);
+    }
+}
 
 public class Program
 {
