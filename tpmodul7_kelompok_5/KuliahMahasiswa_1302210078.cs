077f02fcf2144a8d7ecbb12aea20634c5fb44ac2
herdiardn <121550473+herdiardn@users.noreply.github.com>
2023-04-02 11:42:45 +0700

    menambahkan class KuliahMahasiswa_1302210078

---
+++ tpmodul7_kelompok_5/KuliahMahasiswa_1302210078.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Runtime.CompilerServices;
+using System.Text.Json;
+public class KuliahMahasiswa_1302210078
+{
+    public List<Courses> courses { get; set; }
+
+    public KuliahMahasiswa_1302210078(List<Courses> Course)
+	{
+		this.courses = Course;
+	}
+	public KuliahMahasiswa_1302210078() { }
+}
+
+public class Courses
+{
+	public string name { get; set; }
+	public string code { get; set; }
+
+	public Courses() { }
+	public Courses(string name, string code)
+	{
+		this.name = name;
+		this.code = code;
+	}
+}
+
